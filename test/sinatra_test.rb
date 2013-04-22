01edd98f02bf80eaf4785c762b2ea011515ed211
Josh Holtz <josh@rokkincat.com>
2013-04-22 11:28:29 -0500

    Updated sinatra test and README with HAML and ERB examples

--- test/sinatra_test.rb
+++ test/sinatra_test.rb
@@ -3,20 +3,50 @@ require './lib/html_pager'
 
 enable :inline_templates
 
-get '/' do
+get '/haml-example' do
 	page = params[:page]
 	page = 1 unless page
 
-	@pager = HTMLPager::Pager.new(:route => "/", :page_param_name => "page", :previous_page => "Back yo", :next_page => "Forward yo", :current_page => page.to_i, :show_n_pages => 5, :total_page_count => 10)
+	@pager = HTMLPager::Pager.new(:route => "/haml-example", :page_param_name => "page", :previous_page => "Back yo", :next_page => "Forward yo", :current_page => page.to_i, :show_n_pages => 5, :total_page_count => 10)
 
-	haml :index
+	haml :haml_example
+end
+
+get '/erb-example' do
+	page = params[:page]
+	page = 1 unless page
+
+	@pager = HTMLPager::Pager.new(:route => "/erb-example", :page_param_name => "page", :previous_page => "Back yo", :next_page => "Forward yo", :current_page => page.to_i, :show_n_pages => 5, :total_page_count => 10)
+
+	erb :erb_example
 end
 
 __END__
 
-@@ index
+@@ haml_example
 %html
+	
+	:css
+		.pagination a.current-page { font-weight: bold; }
+
 	%body
 		This is an example
 
 		%div= @pager
+
+@@ erb_example
+<html>
+
+	<style type="text/css">
+		.pagination a.current-page { font-weight: bold; }
+	</style>
+
+	<body>
+		This is an example
+
+		<div>
+			<%= @pager %>
+		</div>
+
+	</body>
+</html>
